598b651bbef53dac2257928303ae83c064570279
1070474647@qq.com <1070474647@qq.com>
2020-06-14 15:38:55 +0800

    学习跨域

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -7,12 +7,27 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
+  data() {
+    return {
+      age:30,
+    }
+  },
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+    let url = "https://www.easy-mock.com/mock/5ee4fb0c3cfbe27ca221648d/example/proxy";
+    axios.get(url).then({
+      
+    })
+    jsonp(url,()=>{
+      
+    })
+  },
 }
 </script>
 
